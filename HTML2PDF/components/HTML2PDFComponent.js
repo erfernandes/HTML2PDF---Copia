262f2fff761fae775e2ff23e8480d13401d549d8
Eduardo Fernandes <eduardofernandes@live.com>
2023-08-04 12:12:58 -0300

    Adição de id no elemento do botão para permitir estilização com CSS

--- HTML2PDF/components/HTML2PDFComponent.js
+++ HTML2PDF/components/HTML2PDFComponent.js
@@ -19,37 +19,12 @@ const options = {
 function App({ appContext }) {
     const componentRef = useRef();
 
-    const handleDownload = useReactToPrint({
-      onPrintError: (error) => console.log(error),
-      content: () => componentRef.current,
-      removeAfterPrint: true,
-      print: async (printIframe) => {
-        const document = printIframe.contentDocument;
-        if (document) {
-          const html = document.getElementById("element-to-print");
-          var opt = {
-              pagebreak: {
-                  mode: ['avoid-all', 'css', 'legacy']
-              },
-              margin:       [10, 5],
-              filename:     'myfile.pdf',
-              image:        { type: 'jpeg', quality: 0.95 },
-              html2canvas:  { useCORS: true },
-              jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' }
-          };
-          const exporter = new Html2Pdf(html, opt);
-          await exporter.getPdf(true);
-        }
-      },
-    });
-
     return(
         <React.Fragment>
             <ReactToPrint
-                trigger={() => <button>Imprimir !</button>}
+                trigger={() => <button id='btnImprimirPDF'>Imprimir</button>}
                 content={() => componentRef.current}
             />
-            <button onClick={handleDownload}>Download</button>
             <div className='pagebreak full-table'>
               <div id="element-to-print" ref={componentRef}>
                   { parse(appContext.parameters.HTMLCode.raw) }
